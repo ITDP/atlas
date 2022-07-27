fecaa2312821c193b08f509f807339f4dd25c5fa
Kaue Braga <kauebraga90@gmail.com>
2022-07-27 10:37:23 -0300

    update setup file

--- setup.R
+++ setup.R
@@ -1,1 +1,15 @@
-# download data etc
+# The script will download the Atlas data and setup the directories
+options(timeout=300)
+
+# create data-raw folder
+dir.create("data-raw")
+
+
+# download data
+download.file("https://github.com/ITDP/atlas/releases/download/mvp1/sample_3.zip",
+              "data-raw/sample_3.zip")
+
+# unpack files
+unzip("data-raw/sample_3.zip",
+      exdir = "data-raw")
+
